26ca43f775c2c9c0366a2cfa403656f47fc3a344
amdewitt <adammichaeldewitt@gmail.com>
2023-07-30 11:51:21 -0400

    Completed Dataset Class

--- src/siamBaseModel.py
+++ src/siamBaseModel.py
@@ -1,5 +1,27 @@
 # The base architecture of the model
 # Class SiameseBaseModel
-#   Constructor
-#   Forward - puts two images through model
-#   Output - Class input is closest to
+    # Constructor
+    # Forward - puts two images through model
+    # Output - Class input is closest to
+
+# Imports
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision import transforms as T
+from torch.optim.lr_scheduler import  StepLR
+
+class SiameseModel(nn.Module):
+    # Constructor
+    def __init__(self)
+        super(SiameseModel, self).__init__()
+        
+        # use resnet for
+        self.resnet = torchvision.models.resnet18(pretrained=False)
+
+        self.
--- src/siamDataset.py
+++ src/siamDataset.py
@@ -2,16 +2,48 @@
 # Manages the dataset, including training, validation, and test sets
 
 # Class Dataset
-#  Constructor(train_csv, train_dir)
-#  Get Item
-#  Return Length
+    # Constructor(csvFile)
+        # CSV File Format: Image0, Image1, label
+    # Get Item
+    # Return Length
 
 # Imports
 
+from PIL import Image
+import pandas as pd
+import numpy as np
+import os
+
 import torch
 import torch.nn as nn
+import torchvison.transforms as transforms
+from torch.utils.data.dataset import Dataset
 
-class SiameseDataset(nn.Module):
-    pass
-    def __init__(self, train_csv)
+class SiameseDataset():
+    def __init__(self, csvFile=None, directory=None, transform=None)
+        self.df = pd.read_csv(csvFile)
+        self.df.columns = ["image1" , "image2" , "label"]
+        self.dir = directory
+        self.transform = transform
+
+    def __getItem__(self, index):
+        # Get image paths
+        image0_path = os.path.join(self.dir, self.df.iat[index, 0])
+        image1_path = os.path.join(self.dir, self.df.iat[index, 1])
+
+        # Load images
+        image0 = Image.open(image0_path)
+        image1 = Image.open(image1_path)
+
+        image0 = image0.convert("L")
+        image1 = image1.convert("L")
+
+        if self.transform is not None:
+            image0 = self.transform(image0)
+            image1 = self.transform(image1)
+        return image0, image1, torch.from_numpy(torch.from_numpy(np.array([int(self.df.iat[index,2])],dtype=np.float32)))
+
+
+    def __len__(self):
+        return len(self.df)
     
