db4b2ad6e6d3d6c28638a683f834c3d4a8927d38
amdewitt <adammichaeldewitt@gmail.com>
2023-07-30 10:44:00 -0400

    Started coding framework, starting with dataset

--- src/siamDataset.py
+++ src/siamDataset.py
@@ -2,7 +2,16 @@
 # Manages the dataset, including training, validation, and test sets
 
 # Class Dataset
-#  Constructor
+#  Constructor(train_csv, train_dir)
 #  Get Item
 #  Return Length
-# Get Label of Two Image Classes (True or False)
+
+# Imports
+
+import torch
+import torch.nn as nn
+
+class SiameseDataset(nn.Module):
+    pass
+    def __init__(self, train_csv)
+    
