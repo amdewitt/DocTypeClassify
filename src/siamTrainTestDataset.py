7623f217bb9b03433be0a79a1aa80a30350517e7
amdewitt <adammichaeldewitt@gmail.com>
2023-08-04 22:00:23 -0400

    X

--- src/siamTrainTestDataset.py
+++ src/siamTrainTestDataset.py
@@ -16,56 +16,41 @@ import numpy as np
 import pandas as pd
 import numpy as np
 import os
-import torch as th
-
-import siamClassDict
-
-#import torch
+import torch
 
 
 class SiameseDataset():
-    # CSV File Format: CSV File Format: Image1, Image2, IsSameClass
+    # CSV File Format: CSV File Format: Image1, Image2, Label
     def __init__(self, csvFile=None, directory=None, transform=None):
         self.df = pd.read_csv(csvFile)
-        self.df.columns = ["image1", "image2", "sameclass"]
+        self.df.columns = ["image1", "image2", "isSimilar"]
         self.dir = directory
         self.transform = transform
 
     # Returns results in following order: Image, Class # of Image
     def __getitem__(self, index):
-        # getting the image path
-        image0_path=os.path.join(self.train_dir,self.train_df.iat[index,0])
-        image1_path=os.path.join(self.train_dir,self.train_df.iat[index,1])
-        # Loading the image
-        img0 = Image.open(image1_path)
-        img1 = Image.open(image2_path)
-        img0 = img0.convert("L")
-        img1 = img1.convert("L")
-        # Apply image transformations
+        # Get image paths
+        image0_path = os.path.join(self.dir, self.df.iat[index, 0])
+        image1_path = os.path.join(self.dir, self.df.iat[index, 1])
+
+        image0 = self.__pathToImage(image0_path)
+        image1 = self.__pathToImage(image1_path)
+        
+        return (
+            image0,
+            image1,
+            torch.from_numpy(
+                np.array([int(self.train_df.iat[index, 2])], dtype=np.float32)
+            )
+        )
+
+    # Path
+    def __pathToImage(self, path):
+        img = Image.open(path)
+        img = img.convert("L")
         if self.transform is not None:
-            img0 = self.transform(img0)
-            img1 = self.transform(img1)
-        return img0, img1 , th.from_numpy(np.array([int(self.train_df.iat[index,2])],dtype=np.float32))
-    
-    """def __getRandomItemFromClass__(self, targetClass):
-        imgList = []
-        nImagesInClass = 0 # number of images in the target class
-        for i in self.df:
-            classQuery = siamClassDict.__findClass__(targetClass)
-            imgPath, imgClass = self.__getSingleItem__(i)
-            if classQuery == imgClass:
-                classImg = [imgPath]
-                imgList.extend[classImg]
-                nImagesInClass += 1
-            else:
-                continue
-        
-        # Gets random index and returns image at that index
-        r = random.randint(0, (nImagesInClass - 1))
-        randImg = imgList[r]
-        return randImg"""
-        
-        #return randImg, imgClass
+            img = self.transform(img)
+        return img
 
     def __len__(self):
         return len(self.df)
