810e327e7a3f08c25a44be5d8b366ab69f2ec788
amdewitt <adammichaeldewitt@gmail.com>
2023-08-04 22:06:10 -0400

    X

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
-training_dir = "/content/drive/My Drive/data/sign_data/train"
 training_csv = "/content/drive/My Drive/data/sign_data/train_data.csv"
+training_dir = "/content/drive/My Drive/data/sign_data/train"
 testing_csv = "/content/drive/My Drive/data/sign_data/test_data.csv"
 testing_dir = "/content/drive/My Drive/data/sign_data/test"
 batch_size = 32
--- src/siamClassify.py
+++
@@ -1,3 +0,0 @@
-# Driver Code for the model
-# takes an input and compares it to a random image from each class
-#
