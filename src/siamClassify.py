f8d96300a3e6a2994fff096da92d2147f13b7ed7
amdewitt <adammichaeldewitt@gmail.com>
2023-08-14 14:23:05 -0400

    X

--- src/siamClassify.py
+++ src/siamClassify.py
@@ -1,7 +1,8 @@
-
+# Main Driver
+# Used for classification of the model
 # Gets input image
 # Runs input image through test with each image in a pointwise dataset
-# Returns the class with the lowest distance
+# Returns the class with the lowest pairwise distance
 def __main__():
     pass
 
