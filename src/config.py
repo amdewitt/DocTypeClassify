cdbe9930b2de1c5b4e15f68d54d4f70caca146bd
amdewitt <adammichaeldewitt@gmail.com>
2023-08-15 11:20:35 -0400

    Added mock dataset (for demo purposes)

--- src/config.py
+++ src/config.py
@@ -8,24 +8,24 @@ import torchvision.transforms as transforms
 # Configuration Variables
 
 # Training
-training_csv = "/sets/train/" # Points to the images in directory that are being used. CSV Format: Image Path (Relative to Root Directory), Class
-training_dir = "/sets/trainCSV.csv" # Root Directory (MUST END IN "/"!!!)
+training_csv = "/sets/demoTrain/" # Points to the images in directory that are being used. CSV Format: Image Path (Relative to Root Directory), Class
+training_dir = "/sets/demoTrainCSV.csv" # Root Directory (MUST END IN "/"!!!)
 
 # Validation
-validation_csv = "/sets/validation/" # Points to the images in directory that are being used. CSV Format: Image Path (Relative to Root Directory), Class
-validation_dir = "/sets/validationCSV.csv" # Root Directory (MUST END IN "/"!!!)
+validation_csv = "/sets/demoValidation/" # Points to the images in directory that are being used. CSV Format: Image Path (Relative to Root Directory), Class
+validation_dir = "/sets/demoValidationCSV.csv" # Root Directory (MUST END IN "/"!!!)
 
 # Testing 
-testing_csv = "/sets/test/" # Points to the images in directory that are being used. CSV Format: Image Path (Relative to Root Directory), Class
-testing_dir = "/sets/testCSV.csv" # Root Directory (MUST END IN "/"!!!)
+testing_csv = "/sets/demoTest/" # Points to the images in directory that are being used. CSV Format: Image Path (Relative to Root Directory), Class
+testing_dir = "/sets/demoTestCSV.csv" # Root Directory (MUST END IN "/"!!!)
 
 # Training and Validation Parameters
 batch_size = 32 # Batch size for training and validation
 epochs = 20 # Number of epochs to train over
 
 # Size to resize images in Transform
-width = 400
-height = 400
+width = 105
+height = 105
 # Image Transform (Used to convert imagaes to a format useable by the network)
 transform = transform=transforms.Compose([transforms.Resize((height, width)), transforms.ToTensor()])
 
@@ -36,7 +36,7 @@ loss_margin = 1.0
 max_tests = 20
 
 # Path used to save and load the model's state
-model_path = "/savedModels/model.pth"
+model_path = "/savedModels/modelDemoWTaikyokuMnemPieces.pth"
 
 # File Types Accepted by Input Dialog
 accepted_input_types = [("PNG files", "*.png"), ("JPG files", "*.jpg"), ("GIF files", "*.gif")]
