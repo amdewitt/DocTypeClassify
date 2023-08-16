0352091a9fdea688929c2661a6d2f1b1ad20e325
amdewitt <adammichaeldewitt@gmail.com>
2023-08-16 14:32:28 -0400

    X

---
+++ src/siamModel.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import tensorflow.keras as keras
+import tensorflow as tf
+import numpy as np
+import random
+from keras.datasets import mnist
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Flatten, Dense, Dropout, Lambda
+from tensorflow.keras.layers import Conv2D, Activation,AveragePooling2D
+from keras import backend as K
+
+class SiamModel():
+    def base_model():
+        pass
+    def train():
+        pass
+    def validate():
+        pass
+    def test():
+        pass
